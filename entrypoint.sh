8705e36c2c0927206fec8862172d8d745e934a1a
Carl Hauschke <carl.hauschke@me.com>
2016-05-16 17:50:33 +0200

    merged entrypoint.sh and docker-startup.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 service php5-fpm start
+
 cd /var/www/whatsspy
-if ![ -s api/config.php ]; then
+
+if ! [ -s api/config.php ]; then
 	cp api/config.example.php api/config.php
 fi
-/var/www/whatsspy/api/tools/controls/docker-startup.sh
+
+LOCKFILE="/var/lock/tracker.lock"
+
+if ( set -o noclobber; echo "locked" > "$LOCKFILE") 2> /dev/null; then
+  	# Start tracker session
+	# Started under the user www-data
+	# Called from the api/ dir.
+	php /var/www/whatsspy/api/tracker.php | tee /var/www/whatsspy/api/tracker.log
+	echo "Started tracker session."
+else
+  echo "Lock failed - exit"
+  exit 1
+fi
