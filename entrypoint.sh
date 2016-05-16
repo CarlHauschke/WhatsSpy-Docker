0ddfb2d5bf83089858e97fde85460690cf440f14
Carl Hauschke <carl.hauschke@me.com>
2016-05-16 13:44:01 +0200

    fixed if

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 service php5-fpm start
 cd /var/www/whatsspy
-if ![ -s api/config.php ] then
+if ![ -s api/config.php ]; then
 	cp api/config.example.php api/config.php
+fi
 /var/www/whatsspy/api/tools/controls/docker-startup.sh
