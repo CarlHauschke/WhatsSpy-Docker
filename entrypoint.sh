22ba96c9c8f511cd46b3fccd2870297a19791475
Carl Hauschke <carl.hauschke@me.com>
2016-05-16 17:59:38 +0200

    start tracker with user "www-data" + simpler start

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,4 @@ if ! [ -s api/config.php ]; then
 	cp api/config.example.php api/config.php
 fi
 
-LOCKFILE="/var/lock/tracker.lock"
-
-if ( set -o noclobber; echo "locked" > "$LOCKFILE") 2> /dev/null; then
-  	# Start tracker session
-	# Started under the user www-data
-	# Called from the api/ dir.
-	php /var/www/whatsspy/api/tracker.php | tee /var/www/whatsspy/api/tracker.log
-	echo "Started tracker session."
-else
-  echo "Lock failed - exit"
-  exit 1
-fi
+sudo -u www-data php /var/www/whatsspy/api/tracker.php | tee /var/www/whatsspy/api/tracker.log
